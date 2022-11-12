ecdefc22c1f5387c631279f13ca1e83d30b568e9
Terry <82017869+2218084076@users.noreply.github.com>
2022-11-12 16:13:13 +0800

    feat: python基本数据类型用法

---
+++ python基础数据类型/while_demo.py
@@ -0,0 +1,16 @@
+"""猜数字"""
+
+# import random
+
+# answer = random.randint(0, 5)
+# counter = 0
+# print(answer)
+# while True:
+#     if counter == answer:
+#         print(f'{counter}=={answer}')
+#         break
+#     if counter < answer:
+#         print('猜小了')
+#     counter += 1
+
+# for
